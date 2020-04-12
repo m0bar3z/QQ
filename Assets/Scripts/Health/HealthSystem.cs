3d9b11118da647b0d5d6581c5055ad6b388705e5
MbZ <amobarez00@gmail.com>
2020-04-12 22:50:10 +0430

    added Health System

---
+++ Assets/Scripts/Health/HealthSystem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthSystem
+{
+    public virtual void Health()
+    {
+
+    }
+   public virtual void Damage()
+    {
+
+    }
+    public virtual void GetAlive()
+    {
+
+    }
+}
