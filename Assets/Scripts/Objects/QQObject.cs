3107858bc3240019fb1688ec61e759781c012804
MH <mre.ezboy3@gmail.com>
2020-04-12 21:05:19 +0330

    Object Code Added

---
+++ Assets/Scripts/Objects/QQObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QQObject : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public virtual void PickUp(PlayerController picker)
+    {
+
+    }
+
+    public virtual void ReceiveForce(Vector2 force)
+    {
+
+    }
+}
