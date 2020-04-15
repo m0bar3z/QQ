97949a806bf6339c1a738b977da149fe923c7c62
MH <mre.ezboy3@gmail.com>
2020-04-15 13:33:34 +0330

    more sfx

---
+++ Assets/Scripts/Utils/DestroyAfter.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfter : MonoBehaviour
+{
+    public float destroyAfter = 2f;
+
+    private void Start()
+    {
+        Destroy(gameObject, destroyAfter);
+    }
+}
---
+++ Assets/Scripts/Utils/SFXPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXPlayer : MonoBehaviour
+{
+    public AudioSource source;
+    public AudioClip[] clips;
+
+    private void Start()
+    {
+        source.PlayOneShot(
+            clips[Random.Range(0, clips.Length)]    
+        );
+    }
+}
