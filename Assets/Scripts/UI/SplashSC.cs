ab2d5de7f84649c24eaff001521e9826d0fda04e
MbZ <amobarez00@gmail.com>
2020-04-16 13:21:27 +0430

    editing menu
    create LevelManager prefab

---
+++ Assets/Scripts/UI/SplashSC.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SplashSC : MonoBehaviour
+{
+    public float time;
+    void Start()
+    {
+        Invoke(nameof(LoadingStartMenu), time);
+    }
+
+    public void LoadingStartMenu()
+    {
+        SceneManager.LoadScene("StartMenu");
+    }
+
+    public void LoadLevel(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+}
