2639417e1c902c9d69603d444bad1adb8ffa43ea
MbZ <amobarez00@gmail.com>
2020-04-16 19:39:24 +0430

    pause menu and pause test scene

---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField]
+    GameObject pausePanel, player;
+
+    public void GamePaused()
+    {
+        pausePanel.SetActive(true);
+        Time.timeScale = 0f;
+        player.SetActive(false);
+    }
+
+    public void ResumeGame()
+    {
+        pausePanel.SetActive(false);
+        Time.timeScale = 1f;
+        player.SetActive(true);
+    }
+    
+    public void Exit()
+    {
+        player.SetActive(true);
+        Application.Quit();
+    }
+    void Start()
+    {
+        pausePanel.SetActive(false);
+    }
+
+
+
+    void Update()
+    {
+        
+    }
+}
