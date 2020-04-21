3df1bd4dcb2c09d418618a576f0f251e18a537a3
MbZ <amobarez00@gmail.com>
2020-04-21 22:18:50 +0430

    adjust score and highest score in game

--- Assets/Scripts/PlayerPrefs/PlayerPrefsManager.cs
+++ Assets/Scripts/PlayerPrefs/PlayerPrefsManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerPrefsManager : MonoBehaviour
 {
     const string MASTER_VOLUME_KEY = "master_volume";
+    const string MASTER_SCORE_KEY = "master_score";
 
     public static void SetMasterVolume(float volume)
     {
@@ -14,6 +15,15 @@ public class PlayerPrefsManager : MonoBehaviour
         }
     }
 
+    public static void SetMasterScore(int score)
+    {
+        PlayerPrefs.SetInt(MASTER_SCORE_KEY, score);
+    }
+
+    public static int GetMasterScore()
+    {
+        return PlayerPrefs.GetInt(MASTER_SCORE_KEY);
+    }
     public static float GetMasterVolume()
     {
         return PlayerPrefs.GetFloat(MASTER_VOLUME_KEY);
