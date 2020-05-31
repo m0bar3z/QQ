7c560353fc576231e8844549295c9da3e1d2b768
MbZ <amobarez00@gmail.com>
2020-05-31 19:39:05 +0430

    set characters volume in menu

---
+++ Assets/Scripts/UI/Menu and LevelManager/CharacterSFXVolume.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSFXVolume : MonoBehaviour
+{
+    public SFXManager sfxManager;
+    public AudioSource audioSource;
+
+    public void SetCharVolume()
+    {
+        sfxManager.SetSFXVoiume();
+    }
+
+    private void Start()
+    {
+        audioSource = gameObject.GetComponent<AudioSource>();
+        sfxManager = FindObjectOfType<SFXManager>();
+        sfxManager.audioSource = audioSource;
+        sfxManager.maxVolume = audioSource.volume;
+        SetCharVolume();
+    }
+}
