5c6f009a8e70c2d2a3fe457ada64bf89c1bca6d5
MbZ <amobarez00@gmail.com>
2020-06-15 18:05:21 +0430

    Barrel (with errors)

---
+++ Assets/Scripts/Barrel/ExplosionFire.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionFire : QQObject
+{
+    protected override void Start()
+    {
+        base.Start();
+        GetComponent<Burnable>().Burn();
+    }
+}
